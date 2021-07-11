91f16bd0d54a2a6777f2e4d5474945916f0bee5b
Subin Shrestha <developer.shrestha@gmail.com>
2021-07-11 12:34:51 +0545

    file name update

--- apps/config/__init__.py
+++ apps/config/__init__.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(__file__)
 sys.path.append(BASE_DIR)
 
 BQ_GCP_PROJECT_ID = ""
-GCP_SERVICE_ACCOUNT_CREDENTIALS = BASE_DIR + "/credentials.json"
+GCP_SERVICE_ACCOUNT_CREDENTIALS = BASE_DIR + "/gcp_credentials.json"
 
 DATAFORSEO_URL = ""
 DATAFORSEO_CREDENTIALS = ["", ""]
